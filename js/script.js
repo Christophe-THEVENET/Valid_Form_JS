82698091676b19324cf7ee0c454d5fc6337e7079
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-05-13 14:48:38 +0200

    ajout fonction switch validation formulaire

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
     form.addEventListener(
         'submit',
         (e) => {
-            // On met dans un tableau tous les éléments (input, select, button) du formulaire
+            // On met dans un tableau tous les éléments (input, select, button) du formulaire elements = mot clé pour les formulaires, il désigne seulement les inputs, select, button, etc....
             // et on boucle dessus pour check
             Array.from(form.elements).forEach((element) => {
                 if (element.type !== 'submit') {
@@ -30,42 +30,85 @@
                 }
             });
         },
-        false
+        false // je crois c'est pour le bouillonnement
     );
-})();
+})(); // la fonction qui s'auto lance (encapsulée ds une autre fonction)
+
+// _____________________________________________________________________
 
 /* ------------------- fonctions de validations générales (vide, nb charactéres, etc...) --------------------------------------------- */
 
 // validation d'un champ REQUIRED
-function validateRequired(input) {
+
+const validateRequired = (input) => {
     return !(input.value == null || input.value == '');
-}
+};
 
 // validation du nombre de caractéres min et max
-/* const validateLength = (input, minLength, maxLength) => {
-    return !(input.value.length < minLength && input.value.length > maxLength);
-}; */
-
-function validateLength(input, minLength, maxLength) {
-    console.log(input.value.length);
+const validateLength = (input, minLength, maxLength) => {
     return !(input.value.length < minLength || input.value.length > maxLength);
-}
+};
+
+// validation d'un input text (Regex)
+const validateText = (input) => input.value.match('^[A-z]+$');
+
+// validation d'un input email (Regex)
+const validateMail = (input) => {
+    let email = input.value;
+    let positionArrobase = email.indexOf('@'); // position de l'@
+    let positionLastPoint = email.lastIndexOf('.'); // position de la derniere occurence '.'
+
+    return !(
+        positionArrobase < 1 ||
+        positionLastPoint - positionArrobase < 2 ||
+        positionLastPoint == email.length - 1
+    );
+};
 
 /* ------------------- fonctions de validations par type d'input --------------------------------------------- */
 
-function validateFields(input) {
+const validateFields = (input) => {
     let fieldName = input.name;
 
     // validation du champ Prénom
-    if (fieldName == 'firstName') {
+    /* if (fieldName == 'firstName') {
         if (!validateRequired(input)) {
             return false;
         }
-
         if (!validateLength(input, 2, 20)) {
             return false;
         }
-
+        if (!validateText(input)) {
+            return false;
+        }
         return true;
     }
-}
+
+    // validation du champ Email
+    if (fieldName == 'email') {
+        if (!validateMail(input)) {
+            return false;
+        }
+    } */
+
+    switch (fieldName) {
+        // validation du champ Prénom
+        case 'firstName':
+            if (!validateRequired(input)) {
+                return false;
+            }
+            if (!validateLength(input, 2, 20)) {
+                return false;
+            }
+            if (!validateText(input)) {
+                return false;
+            }
+            return true;
+        // validation du champ Email
+        case 'email':
+            if (!validateMail(input)) {
+                return false;
+            }
+            return true;
+    }
+};
